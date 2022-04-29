8c9d14aa8bf64aaae4c46f525b96b49359e6b61e
kweinert <kweinert@users.noreply.github.com>
2022-04-29 12:30:05 +0200

    removed |>

--- R/reply2.R
+++ R/reply2.R
@@ -1,7 +1,7 @@
 reply2 <- function(w, allowed) {
-	prob <- repm[w,allowed] |> 
-		table() |>
-		as.vector()
+	prob <- repm[w,allowed] 
+	prob <- table(prob)
+	prob <- as.vector(prob)
 	prob <- prob / sum(prob)
 	entr <- -sum(prob*log2(prob))
 	return(entr)
